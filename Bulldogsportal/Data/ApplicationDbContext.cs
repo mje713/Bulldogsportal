d331e81e3fc52ea9148d9ff59122ab64245f550b
mje713 <chewyuing@gmail.com>
2022-11-08 21:56:33 -0500

    Add project files.

---
+++ Bulldogsportal/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Bulldogsportal.Models.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulldogsportal.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<RSVP> Rsvp { get; set; }
+    }
+}
---
+++ Bulldogsportal/Models/Domain/RSVP.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+
+namespace Bulldogsportal.Models.Domain
+{
+    public class RSVP
+    {
+        public string Callsign { get; set; }
+        private string Email { get; set; }
+        public string Airframe { get; set; }
+    }
+}
